7076232c00f4bc6200bae153b3a4743e6d4be934
An-MinSu <dmdkrgkrrydp@naver.com>
2021-12-07 11:28:49 +0900

    2021-12-07

---
+++ Number theory and combination theory/5086.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main{
+	public static void main(String[] args)throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		int a=-1;
+		int b=-1;
+		while(true) {
+
+			st = new StringTokenizer(br.readLine()," ");
+			a = Integer.parseInt(st.nextToken());
+			b = Integer.parseInt(st.nextToken());
+			
+			if(a==0 && b==0)
+				break;
+			else if(b%a == 0)
+				sb.append("factor\n");
+			else if(a%b==0)
+				sb.append("multiple\n");
+			else
+				sb.append("neither\n");
+		}
+		System.out.print(sb);
+	}
+}
