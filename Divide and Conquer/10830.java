8a1db73680610669a99b280e067b6e469eb84e28
An-MinSu <dmdkrgkrrydp@naver.com>
2022-02-05 16:03:33 +0900

    행렬 곱셈 수정

--- Divide and Conquer/10830.java
+++ Divide and Conquer/10830.java
@@ -19,7 +19,7 @@ public class Main{
 		for(int i=0;i<N;i++) {
 			st = new StringTokenizer(br.readLine()," ");
 			for(int j=0;j<N;j++) {
-				list[i][j] = Integer.parseInt(st.nextToken());
+				list[i][j] = Integer.parseInt(st.nextToken()) %1000;
 			}
 		}
 		
