938028b8092476956f553013eb7ded3aaf4e88b6
An-MinSu <dmdkrgkrrydp@naver.com>
2022-02-23 12:31:36 +0900

    파일 합치기

---
+++ Dynamic Programming2/11066.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main{
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		while(T-->0) {
+			int K = Integer.parseInt(br.readLine());
+			int[][] dp = new int[K+1][K+1];
+			int[] file = new int[K+1];
+			int[] sum = new int[K+1];
+			
+			st = new StringTokenizer(br.readLine());
+			
+			for(int i=1;i<=K;i++) {
+				file[i] = Integer.parseInt(st.nextToken());
+				sum[i] = sum[i-1]+file[i];
+			}
+			
+			for(int i=1;i<=K;i++) {
+				for(int from=1;from+i<=K;from++) {
+					int to = from+i;
+					dp[from][to] = Integer.MAX_VALUE;
+					for(int div=from;div<to;div++) {
+						dp[from][to] = Math.min(dp[from][to],dp[from][div]+dp[div+1][to]+sum[to]-sum[from-1]);
+					}
+				}
+			}
+			sb.append(dp[1][K]).append("\n");
+		}
+		System.out.println(sb);
+	}
+}
