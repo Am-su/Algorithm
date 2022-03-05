bbc408c8d96f5c30639dfa5a3931f0e39bf6d18d
An-MinSu <dmdkrgkrrydp@naver.com>
2022-03-05 14:57:36 +0900

    오븐 시계

---
+++ If/2525.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main{
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int h = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		int plus = Integer.parseInt(br.readLine());
+		
+		while(plus-->0) {
+			m++;
+			if(m>59) {
+				m=0;
+				h++;
+				if(h>23) {
+					h=0;
+				}
+			}
+		}
+		System.out.println(h+" "+m);
+	}
+}
